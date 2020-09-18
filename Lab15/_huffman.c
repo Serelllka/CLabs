7d159245b60c6ea7349e4da767c4b1a21999215d
Serellka <44942855+Serellka@users.noreply.github.com>
2020-09-18 11:34:45 +0300

    Update _huffman.c

--- Lab15/_huffman.c
+++ Lab15/_huffman.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <math.h>
 
-
 struct Node
 {
     int a;
@@ -11,17 +10,27 @@ struct Node
     struct Node *left, *right;
 };
 
-int convert (int n, int t)
+int reverse (int x)
 {
-    int k = 0;
     int ans = 0;
-    while (n != t)
+    for (int i = 0; i < 8; ++i)
+    {
+        ans = ans*2 + x%2;
+        x /= 2;
+    }
+    return ans;
+}
+
+int convert (int n)
+{
+    int k = 0, ans = 0;
+    while (n != 1)
     {
         ans = ans * 2 + n % 2;
         n /= 2;
         k++;
     }
-    if (t) ans += pow(2, k);
+    ans += pow(2, k);
     return ans;
 }
 
@@ -55,40 +64,53 @@ struct Node* join (struct Node* left, struct Node* right)
     return c;
 };
 
+void Compression (char *inputFileName, char *outputFileName)
+{
+    FILE* inputFile;
+    FILE* outputFile;
+}
 
-int main()
+int main(int argc, char* argv[])
 {
     char* dict = (char*)malloc(256 * sizeof(char));
     int set[256];
     for (int i = 0; i < 256; ++i)
         set[i] = 0;
-    //int setsize = 0;
 
-    char *inputfilename = "input.txt", *outputfilename = "output.txt", c;
+    char *inputFileName = "input.txt",
+        *outputFileName = "output.txt",
+        *archiveName = "output.txt",
+        *filenametodecode = "input1.txt";
+    int c;
 
-    FILE* inputfile;
-    FILE* outputfile;
+    FILE* inputFile;
+    FILE* outputFile;
+    FILE* archive;
+    FILE* decodedFile;
 
     struct Node** tree = (struct Node**)malloc(256 * sizeof(struct Node*));
 
     int frequency[256];
-    for (int i = 20; i < 256; ++i)
+    for (int i = 0; i < 256; ++i)
         frequency[i] = 0;
 
-    if ((inputfile = fopen(inputfilename, "rb")) == NULL)
-    {
-        printf ("Enter correct filename to read!\n");
-        return 0;
-    }
-    outputfile = fopen(outputfilename, "wb");
+    //for (int i = 0; i < argc; ++i)
+    //{
+        if ((inputFile = fopen(inputFileName, "rb")) == NULL)
+        {
+            printf ("Enter correct filename to read!\n");
+            return 0;
+        }
+    //}
+    outputFile = fopen(outputFileName, "wb");
 
-    while((c = fgetc(inputfile)) != EOF)
+    while((c = getc(inputFile)) != EOF)
     {
 		frequency[(unsigned char)c]++;
 	}
 
     int k = 0;
-	for (int i = 20; i < 256; ++i)
+	for (int i = 0; i < 256; ++i)
     {
         if (frequency[i] != 0)
         {
@@ -103,23 +125,19 @@ int main()
 
     bubble_sort (tree, k);
 
-    int size_of_pair_mas = 0;
-
     while (k != 1)
     {
+        bubble_sort (tree, k);
         tree[k-2] = join(tree[k - 2], tree[k - 1]);
         k--;
-        bubble_sort (tree, k);
     }
 
-    void next (struct Node* vertex, int t)
+    void next(struct Node* vertex, int t)
     {
         if (vertex->right == NULL)
         {
             dict[(int)vertex->c] = t;
-            printf("%c %d %d\n", vertex->c, dict[(int)vertex->c], (int)vertex->c);
             set[t] = (int)vertex->c;
-            size_of_pair_mas++;
             return;
         }
         next (vertex->right, t * 2 + 1);
@@ -128,16 +146,15 @@ int main()
 
     next(tree[0], 1);
 
-    fseek (inputfile, 0, SEEK_SET);
+    fseek (inputFile, 0, SEEK_SET);
 
-    int count = 0, number = 1, overall = 0;
+    int count = 0, number = 1;
     char* str = (char*)malloc(8 * sizeof(char));
     strcpy(str, "");
 
-    while((c = fgetc(inputfile)) != EOF)
+    while((c = getc(inputFile)) != EOF)
     {
-        int x = convert(dict[c], 1);
-        //printf ("%d", x);
+        int x = convert(dict[c]);
         while (x != 1)
         {
             if (x % 2 == 1)
@@ -146,58 +163,77 @@ int main()
                 strcat(str, "0");
             x = x >> 1;
             count += 1;
-            overall += 1;
             if (count == 8)
             {
                 int sum = 0;
                 for (int i = 0; i < 8; ++i)
                     sum += (str[i] - '0') * pow(2, 7 - i);
-                printf ("%s\n", str);
-                fprintf (outputfile, "%c", (char)sum);
+                putc ((char)sum, outputFile);
                 count = 0;
-                //printf ("%s\n", str);
                 strcpy(str, "");
             }
         }
-        //printf("%d\n", x);
 	}
 
-	for (int i = 0; i < 256; ++i)
+    int sum = 0;
+    if (count != 0)
     {
-        if (set[i]) printf ("%d\n", i);
+        for (int i = 0; i < count; ++i)
+            sum += (str[i] - '0') * pow(2, 7 - i);
+        putc ((char)sum, outputFile);
     }
 
-    fprintf (outputfile, "%d", overall);
+	putc (8 - count, outputFile);
 
-    fclose(inputfile);
-    fclose(outputfile);
+    fclose(inputFile);
+    fclose(outputFile);
 
-    char *filenametoopen = "output.txt";
-    FILE* openfile;
+    archive = fopen (archiveName, "rb");
+    decodedFile = fopen (filenametodecode, "wb");
 
-    openfile = fopen (filenametoopen, "rb");
     int t = 1;
     char* ans[256];
     strcpy (ans, "");
-    while((c = fgetc(inputfile)) != EOF)
+    c = getc(inputFile);
+    int ft[2];
+    ft[1] = c;
+    c = fgetc(inputFile);
+    while(1)
     {
-		int x = (unsigned char)c;
-		//printf("%d|\n", x);
-		x = convert(x, 0);
-		//printf("%d|\n", x);
+        ft[0] = ft[1];
+        ft[1] = c;
+        c = getc(inputFile);
+        if (c == EOF) break;
+		int x = (unsigned char)ft[0];
+		x = reverse(x);
 		for (int i = 0; i < 8; ++i)
         {
             t = t*2 + x%2;
             x /= 2;
-            if (set[t]!=0)
+            if (set[t] != 0)
             {
                 char* ch[1];
-                ch[0] = (char)set[t];
+                ch[0] = (char*)set[t];
                 strcat (ans, ch);
+                putc (set[t], decodedFile);
                 t = 1;
             }
         }
 	}
-	printf("%s\n", ans);
 
+    int x = (unsigned char)ft[0];
+    x = reverse(x);
+	for (int i = 0; i < 8 - (ft[1] % 8); ++i)
+        {
+            t = t*2 + x%2;
+            x /= 2;
+            if (set[t] != 0)
+            {
+                char* ch[1];
+                ch[0] = (char*)set[t];
+                strcat (ans, ch);
+                putc (set[t], decodedFile);
+                t = 1;
+            }
+        }
 }
