b78926704a982b6eb44a7004b95702ccf0049b43
Serellka <44942855+Serellka@users.noreply.github.com>
2020-10-19 00:45:44 +0300

    Update Lab10.c

--- Lab10/Lab10.c
+++ Lab10/Lab10.c
@@ -6,17 +6,17 @@
 Lab 10
 **/
 
-double lenght (double x1, double y1, double x2, double y2)
+float lenght (float x1, float y1, float x2, float y2)
 {
     return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 
-int** dist (int dotsX[], int dotsY[], int n)
+int** dist (float dotsX[], float dotsY[], int n)
 {
-    int **matr = (int**)malloc(n * sizeof(int*));
+    float **matr = (float**)malloc(n * sizeof(float*));
     for (int i = 0; i < n; ++i)
     {
-        matr[i] = (int*)malloc(n * sizeof(int));
+        matr[i] = (float*)malloc(n * sizeof(float));
     }
 
     for (int i = 0; i < n; ++i)
@@ -30,7 +30,7 @@ int** dist (int dotsX[], int dotsY[], int n)
     return matr;
 }
 
-int* toMas (int x)
+int* toMas (int x, int *t)
 {
     int k = 0;
     int a = x;
@@ -45,26 +45,45 @@ int* toMas (int x)
         mas[i] = x % 10;
         x /= 10;
     }
+    *t = k;
     return mas;
 }
 
 int main()
 {
-    int dotsX[1000];
-    int dotsY[1000];
+    float dotsX[1000];
+    float dotsY[1000];
     int n, x;
     printf ("Enter count  of dots:\n");
     scanf ("%d", &n);
 
-    for (int i = 0; i < n; ++i)
+    int i, j;
+    for (i = 0; i < n; ++i)
     {
-        printf ("Enter dot number %d (x y):\n", i);
-        scanf ("%d %d", &dotsX[i], &dotsY[i]);
+        printf ("Enter dot number %d (x y):\n", i + 1);
+        scanf ("%f %f", &dotsX[i], &dotsY[i]);
     }
 
     printf ("Enter value:\n");
     scanf ("%d", &x);
 
-    int** matr = dist (&dotsX, &dotsY, n);
-    int* mas = toMas (x);
+    int t = 0;
+    float** matr = dist (&dotsX, &dotsY, n);
+    int* mas = toMas (x, &t);
+
+    for (i = 0; i < n; ++i)
+    {
+        printf ("%d :", i + 1);
+        for (j = 0; j < n; ++j)
+        {
+            printf ("%f ", matr[i][j]);
+        }
+        printf ("\n");
+    }
+
+    for (i = 0; i < t; ++i)
+    {
+        printf ("%d ",  mas[t - i - 1]);
+    }
+
 }
